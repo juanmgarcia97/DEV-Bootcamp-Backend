adee184121d34e563cc1b87c77d53ccd2156a398
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-05-08 16:24:34 -0500

    pawns added to the board

--- chess/src/domain/board.ts
+++ chess/src/domain/board.ts
@@ -1,6 +1,7 @@
 import Bishop from './bishop';
 import King from './king';
 import Knight from './knight';
+import Pawn from './pawn';
 import Queen from './queen';
 import Rook from './rook';
 
@@ -13,6 +14,14 @@ export default class Board {
   private w2Knight: Knight;
   private w1Rook: Rook;
   private w2Rook: Rook;
+  private w1Pawn: Pawn;
+  private w2Pawn: Pawn;
+  private w3Pawn: Pawn;
+  private w4Pawn: Pawn;
+  private w5Pawn: Pawn;
+  private w6Pawn: Pawn;
+  private w7Pawn: Pawn;
+  private w8Pawn: Pawn;
 
   private bKing: King;
   private bQueen: Queen;
@@ -22,6 +31,14 @@ export default class Board {
   private b2Knight: Knight;
   private b1Rook: Rook;
   private b2Rook: Rook;
+  private b1Pawn: Pawn;
+  private b2Pawn: Pawn;
+  private b3Pawn: Pawn;
+  private b4Pawn: Pawn;
+  private b5Pawn: Pawn;
+  private b6Pawn: Pawn;
+  private b7Pawn: Pawn;
+  private b8Pawn: Pawn;
 
   constructor() {
     this.wKing = new King('White', 'E', 1);
@@ -32,6 +49,14 @@ export default class Board {
     this.w2Knight = new Knight('White', 'G', 1);
     this.w1Rook = new Rook('White', 'A', 1);
     this.w2Rook = new Rook('White', 'H', 1);
+    this.w1Pawn = new Pawn('White', 'A', 2);
+    this.w2Pawn = new Pawn('White', 'B', 2);
+    this.w3Pawn = new Pawn('White', 'C', 2);
+    this.w4Pawn = new Pawn('White', 'D', 2);
+    this.w5Pawn = new Pawn('White', 'E', 2);
+    this.w6Pawn = new Pawn('White', 'F', 2);
+    this.w7Pawn = new Pawn('White', 'G', 2);
+    this.w8Pawn = new Pawn('White', 'H', 2);
 
     this.bKing = new King('Black', 'E', 8);
     this.bQueen = new Queen('Black', 'D', 8);
@@ -41,5 +66,13 @@ export default class Board {
     this.b2Knight = new Knight('Black', 'G', 8);
     this.b1Rook = new Rook('Black', 'A', 8);
     this.b2Rook = new Rook('Black', 'H', 8);
+    this.b1Pawn = new Pawn('Black', 'A', 7);
+    this.b2Pawn = new Pawn('Black', 'B', 7);
+    this.b3Pawn = new Pawn('Black', 'C', 7);
+    this.b4Pawn = new Pawn('Black', 'D', 7);
+    this.b5Pawn = new Pawn('Black', 'E', 7);
+    this.b6Pawn = new Pawn('Black', 'F', 7);
+    this.b7Pawn = new Pawn('Black', 'G', 7);
+    this.b8Pawn = new Pawn('Black', 'H', 7);
   }
 }
